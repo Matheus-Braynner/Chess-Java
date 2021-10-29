543e43c9a0c2a4a61a11a061b3c00c3f6a23a45a
Matheus Braynner <m.braynner@outlook.com>
2021-10-29 16:09:13 -0300

    Fixed some bugs

--- chess-system/src/chess/ChessPiece.java
+++ chess-system/src/chess/ChessPiece.java
@@ -22,14 +22,14 @@ public abstract class ChessPiece extends Piece {
 		return moveCount;
 	}
 	
-	public void increaseMoveCount() {
+	protected void increaseMoveCount() {
 		moveCount++;
 	}
-	
-	public void decreaseMoveCount() {
-		moveCount++;
+
+	protected void decreaseMoveCount() {
+		moveCount--;
 	}
-	
+
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
 	}
